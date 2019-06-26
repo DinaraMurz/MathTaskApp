aebc34b56a23f20b0ff32cafe0b514811be06aff
Доллар Музыка <dollarmudic@gmail.com>
2019-06-26 18:24:19 +0600

    Добавьте файлы проекта.

---
+++ MathTaskApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 300;  // количество циклов в факториале 
+            int factorial = 1;   // значение факториала 
+
+            for(int i = 0;i < 9999999; i++)
+            {
+                FindFactorial(n, factorial);
+            }
+
+            Console.WriteLine(factorial);
+        }
+
+        static public void FindFactorial(int n, int fact)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+        }
+
+    }
+}
